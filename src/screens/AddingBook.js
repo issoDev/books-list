bb6c942a337feea06f0d018e0bc1fe49031bb5c2
issoDev <issife@hotmail.fr>
2020-12-14 22:25:38 +0100

    adding app screens

---
+++ src/screens/AddingBook.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function AddingBook() {
+  return (
+    <View>
+      <Text>Adding Book</Text>
+    </View>
+  )
+}
