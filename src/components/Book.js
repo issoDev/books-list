9061c5b276fc33dd99e3282f683d6c0cfcdf934f
issoDev <issife@hotmail.fr>
2020-12-15 15:32:07 +0100

    update file

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,14 +4,14 @@ import { Text, View, TouchableOpacity } from 'react-native';
 import { styles } from '../../styles/styles';
 import { Entypo } from '@expo/vector-icons';
 
-export default function Book() {
+export default function Book({ title, author }) {
   return (
     <View style={styles.bookCardContainer}>
       <TouchableOpacity>
         <View style={styles.bookCard}>
           <View style={styles.bookDetails}>
-            <Text style={styles.authorTxtStyle}>Antoine de Saint-Exupéry</Text>
-            <Text style={styles.titleTxtStyle}>Le petit prince</Text>
+            <Text style={styles.authorTxtStyle}> {title} </Text>
+            <Text style={styles.titleTxtStyle}> { author } </Text>
           </View>
           <Entypo name="arrow-with-circle-right" size={24} color="white" />
         </View>
--- src/screens/BookList.js
+++ src/screens/BookList.js
@@ -1,22 +1,54 @@
-import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, TouchableOpacity, FlatList } from 'react-native';
 
 import { styles } from '../../styles/styles';
 import { AntDesign } from '@expo/vector-icons'; 
 import Book from '../components/Book';
 
 export default function BookList({ navigation }) {
+  const [books, setBooks] = useState([
+    { id: Date.now().toString() + 1, title: 'Le petit prince', author: 'Antoine de Saint-Exupéry' }
+  ]);
+
+  const addBook = (title, author) => {
+    setBooks(prevBooks => [...prevBooks, {
+      id: Date.now().toString(),
+      title,
+      author
+    }])
+  }
+
+  const showContent = () => {
+    if(books.length === 0) {
+      return (
+        <View style={styles.titleContainer}>
+          <Text style={styles.titleStyle}>The list of books is empty </Text>
+        </View>
+      )
+    } else {
+      return (
+        <FlatList 
+          data={books}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => {
+            return(
+              <Book 
+                title={item.title}
+                author={item.author}
+              />
+            )
+          }}
+        />
+      )
+    }
+  }
 
   return (
     <View style={styles.container}>
-      <Book />
-      <Book />
-      <View style={styles.titleContainer}>
-        <Text style={styles.titleStyle}>The list of books is empty </Text>
-      </View>
+      { showContent() }
       <TouchableOpacity 
         style={styles.bookListBtn}
-        onPress={() => navigation.push("AddingBook")}
+        onPress={() => navigation.navigate("AddingBook", { addBook })}
       >
         <AntDesign name="plus" size={15} color="white" />
         <Text style={styles.bookListTxtBtn}> add a new book </Text>
