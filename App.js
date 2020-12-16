a1dd1ef1096a1e631ea295c63239d9ec0f54a857
issoDev <issife@hotmail.fr>
2020-12-16 21:40:44 +0100

    update file

--- App.js
+++ App.js
@@ -1,28 +1,15 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { View, Text } from 'react-native';
 
-import BookList from './src/screens/BookList';
-import AddingBook from './src/screens/AddingBook';
 
-const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="BookList">
-        <Stack.Screen 
-          name="BookList"
-          component={BookList}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen 
-          name="AddingBook"
-          component={AddingBook}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    <View> 
+      <Text> Hello </Text>
+    </View>
+  )
 }
 
