995ffd26924510da5ed1b044f63fec12940e84e1
issoDev <issife@hotmail.fr>
2020-12-16 21:45:35 +0100

    update style

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
 import { View, Text } from 'react-native';
+import { styles } from './styles/styles';
 
 
 
 export default function App() {
   return (
-    <View> 
+    <View style={styles.container}> 
       <Text> Hello </Text>
     </View>
   )
--- styles/styles.js
+++ styles/styles.js
@@ -2,5 +2,10 @@ import { StatusBar, Platform, StyleSheet } from 'react-native';
 import colors from './colors'
 
 export const styles = StyleSheet.create({
-  
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+  },
 });
